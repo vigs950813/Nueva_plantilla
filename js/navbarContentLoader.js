eea182b0be57de025a51765eeec95ba8c0298914
vigs950813 <renovatio_bluesunset@hotmail.com>
2025-01-08 16:52:56 -0600

    Prueba de rutas para nacbar y footer en contenido

---
+++ js/navbarContentLoader.js
@@ -0,0 +1,21 @@
+ // Función para cargar contenido dinámico
+ async function loadContent(id, filePath) {
+    try {
+      const response = await fetch(filePath);
+      if (!response.ok) {
+        throw new Error(`Error al cargar ${filePath}: ${response.statusText}`);
+      }
+      const content = await response.text();
+      document.getElementById(id).innerHTML = content;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const navbar = document.getElementById('navbar')
+
+  // Cargar header y foote rdinámicamente
+  loadContent('navbar', 'dynamic/navbarContent.html');
+  //loadContent('footer', './dynamic/footer.html');
+
+  
